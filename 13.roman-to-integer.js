677116afabe18ff9b973b051e78c04cf00ddf8ba
liujia02 <bit_liujia@163.com>
2019-03-12 17:15:21 +0800

    add problem 5

--- 13.roman-to-integer.js
+++ 13.roman-to-integer.js
@@ -82,6 +82,31 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const exclude = {
+        IV: 4,
+        IX: 9,
+        XL: 40,
+        XC: 90,
+        CD: 400,
+        CM: 900
+    }
+    const romanMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    return s.split('').reduce((value,item,index,arr)=>{
+        const twoWords = item + arr[index+1]
+        if(exclude[twoWords]) {
+            arr.splice(index+1,1)
+            return value + exclude[twoWords]
+        } else {
+            return value + romanMap[item]
+        }
+    },0)
 };
 
---
+++ 14.longest-common-prefix.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=14 lang=javascript
+ *
+ * [14] Longest Common Prefix
+ *
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (33.05%)
+ * Total Accepted:    418.3K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * Write a function to find the longest common prefix string amongst an array
+ * of strings.
+ * 
+ * If there is no common prefix, return an empty string "".
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * 
+ * 
+ * Note:
+ * 
+ * All given inputs are in lowercase letters a-z.
+ * 
+ */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    
+};
+
