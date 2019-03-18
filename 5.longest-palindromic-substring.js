159c2a9114dd8c4646ceb73c55961e8b43e6e606
liujia02 <bit_liujia@163.com>
2019-03-18 18:05:00 +0800

    add 5

---
+++ 5.longest-palindromic-substring.js
@@ -0,0 +1,147 @@
+/*
+ * @lc app=leetcode id=5 lang=javascript
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (26.69%)
+ * Total Accepted:    492.1K
+ * Total Submissions: 1.8M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, find the longest palindromic substring in s. You may
+ * assume that the maximum length of s is 1000.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome1 = function(s) {
+    // 扩散法
+    let output = '';
+    function findSinglePalindrome(s,index){
+        let singleOutput = s[index]
+        for(let i=1;i<s.length/2;i++) {
+            const left =  s[(index-i)];
+            const right = s[(index+i)];
+            if(left!==undefined && 
+            right!==undefined
+            && left === right) {
+                singleOutput = left + singleOutput + right
+            } else {
+                break
+            }
+        }
+        return singleOutput
+    }
+    function findDoublePalindrome(s,index){
+        const preIndex = s[(index)];
+        const backIndex = s[(index+1)];
+        let doubleOutput = ''
+        if(preIndex!==undefined && backIndex!==undefined && preIndex===backIndex) {
+            doubleOutput =  preIndex + backIndex
+        } else {
+            return doubleOutput;
+        }
+        for(let i=1;i<s.length/2;i++) {
+            const left =  s[(index-i)];
+            const right = s[(index+1+i)];
+            if(left!==undefined && 
+            right!==undefined
+            && left === right) {
+                doubleOutput = left + doubleOutput + right
+            } else {
+                break
+            }
+        }
+        return doubleOutput
+    }
+    for(let i=0;i<s.length;i++) {
+        const single = findSinglePalindrome(s,i);
+        const double = findDoublePalindrome(s,i);
+        output = single.length > output.length ? single : output
+        output = double.length > output.length ? double : output
+    }
+    return output
+};
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    // 传说中的马拉车法
+    // 首先插入一个#
+    let newstr = '#';
+    for(let i=0;i<s.length;i++) {
+        newstr += s[i]+'#'
+    }
+    // 定义一些变量
+
+    // 以newStr每个字符串为中心的回文串的长度数组，最小为1
+    let len = [];
+    // 最长的回文串的右端点
+    let longestPalindromeRight = 0;
+    // 最长的回文串的中心点
+    let longestPalindromeCenter = 0;
+    // 记录最长回文长度
+    let longestPalindrome = 0;
+
+    for(let i=0;i<newstr.length;i++) {
+        let needExpand = true;
+        let spandStart = i+1;
+        // 在右边界内的时候寻找对称点
+        if(i<longestPalindromeRight) {
+            // 找关于中心的对称点j
+            const j = 2*longestPalindromeCenter - i;
+            // 如果这时候还在右边界内，则不需要扩展
+            if(longestPalindromeRight >= i + len[j] ) {
+                len[i] = len[j];
+                needExpand = false
+            } else {
+                spandStart = longestPalindromeRight + 1
+            }
+        }
+        // 这里是拓展的代码
+        if(needExpand) {
+            // 这里雷同上面的single拓展
+            while(spandStart<newstr.length && 2*i-spandStart>=0) {
+                if(newstr[spandStart] === newstr[2*i-spandStart]) {
+                    spandStart++
+                } else {
+                    break
+                }
+            }
+        }
+        len[i] = spandStart - i;
+        if(len[i]>longestPalindrome) {
+            longestPalindromeRight = spandStart - 1;
+            longestPalindrome = len[i];
+            longestPalindromeCenter = i;
+        }
+    }
+    // 找到len中的最大值索引
+    const originRight = longestPalindromeRight / 2;
+    const originLongestPalindrome = longestPalindrome - 1;
+    return s.substr(originRight-originLongestPalindrome,originLongestPalindrome)
+};
+
+console.log(longestPalindrome('"babad"'))
