7cbb173e5d6f3a94542ab8b01a2c77abc437b67d
liujia02 <bit_liujia@163.com>
2019-04-19 14:32:51 +0800

    add problem 66

---
+++ 66.plus-one.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=66 lang=javascript
+ *
+ * [66] Plus One
+ *
+ * https://leetcode.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (40.72%)
+ * Total Accepted:    376K
+ * Total Submissions: 917.2K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a non-empty array of digits representing a non-negative integer, plus
+ * one to the integer.
+ * 
+ * The digits are stored such that the most significant digit is at the head of
+ * the list, and each element in the array contain a single digit.
+ * 
+ * You may assume the integer does not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ * 
+ */
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let count = 1
+    for(let i = digits.length-1;i>=0;i--) {
+        const value = digits[i] + count
+        if(value === 10) {
+            digits[i] = 0
+            count = 1
+        } else {
+            digits[i] = value
+            count = 0
+        }
+    }
+    if(count === 1) {
+        digits.unshift(1)
+    }
+    return digits
+};
