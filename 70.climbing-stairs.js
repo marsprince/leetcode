cda6a2796f6dfc0701d49d518c03385dd42e254a
liujia02 <bit_liujia@163.com>
2019-04-23 15:14:42 +0800

    add problem 70

---
+++ 70.climbing-stairs.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=70 lang=javascript
+ *
+ * [70] Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (43.54%)
+ * Total Accepted:    386.5K
+ * Total Submissions: 879.9K
+ * Testcase Example:  '2'
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * 
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+ * you climb to the top?
+ * 
+ * Note: Given n will be a positive integer.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ * 
+ * 
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    // dp[i] = dp[i-1] + dp[i-2]
+    if(n <= 2) return n;
+    let a = 1, b = 2;
+    for(let i = 2; i < n; i++) {
+        let tmp = a + b;
+        a = b;
+        b = tmp;
+    }
+    return b;
+};
+
