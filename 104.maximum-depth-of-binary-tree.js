bc547f6c80509421dcce9df8ceea5cf4bbdbc44f
liujia02 <bit_liujia@163.com>
2019-04-25 14:41:36 +0800

    add problem 104

---
+++ 104.maximum-depth-of-binary-tree.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=104 lang=javascript
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(root===null) return 0
+    return 1 + Math.max(maxDepth(root.left),maxDepth(root.right))
+};
+
